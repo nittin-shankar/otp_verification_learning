b99be098394cd84382496e75eca1703d7f8c07c3
Nittin Shankar <nittin.shankar@gmail.com>
2021-01-20 14:51:46 +0530

    Keep default of `otp_last` as integer

--- priv/repo/migrations/20210120090943_create_users.exs
+++ priv/repo/migrations/20210120090943_create_users.exs
@@ -4,7 +4,7 @@ defmodule OTPVerification.Repo.Migrations.CreateUsers do
   def change do
     create table(:users) do
       add :otp_secret, :string
-      add :otp_last, :integer
+      add :otp_last, :integer, default: 0
       add :name, :string
       add :trivia, :string
 
